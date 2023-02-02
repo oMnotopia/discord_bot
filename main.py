372974069690ad2b1503fb2845e9a48b01d800f3
Omnotopia <persinger.clayton@gmail.com>
2023-02-02 13:23:41 -0800

    add study session time and break reminder to study channel

--- main.py
+++ main.py
@@ -1,23 +1,94 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from jokes import jokes
 from list_of_banned_words import curseWords
 import os
 import random
 from dotenv import load_dotenv
+from dataclasses import dataclass
 
 load_dotenv()
 
+STUDYING_CHANNEL_ID = 1070770440525598751
+MAX_SESSION_TIME = 45
+
+
+@dataclass
+class Session:
+    is_active: bool = False
+    start_time: int = 0
+    stop_time: int = 0
+
+
 intents = discord.Intents().all()
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 token = os.getenv('TOKEN')
+
+session = Session()
 
 
 @bot.event
 async def on_ready():
     # Ensures bot is working on run
     print("Logged in as a bot, {0.user}".format(bot))
+
+
+@bot.command()
+async def joke(ctx):
+    channel = str(ctx.channel.name)
+    if (channel == 'studying'):
+        return
+
+    await ctx.send(random.choice(jokes))
+
+
+@bot.command()
+async def start(ctx):
+    channel = str(ctx.channel.name)
+    if (channel != 'studying'):
+        return
+
+    if session.is_active:
+        await ctx.send("A session is already started!")
+        return
+
+    # Starts break reminder execution
+    break_reminder.start()
+
+    session.is_active = True
+    session.start_time = ctx.message.created_at.timestamp()
+    human_readable_start_time = ctx.message.created_at.strftime("%H:%M:%S")
+    await ctx.send(f'Sessions started at {human_readable_start_time}')
+
+
+@bot.command()
+async def stop(ctx):
+    channel = str(ctx.channel.name)
+    if (channel != 'studying'):
+        return
+
+    if not session.is_active:
+        await ctx.send("A session is not started!")
+        return
+
+    # Stops break reminder execution
+    break_reminder.stop()
+
+    session.is_active = False
+    session.stop_time = ctx.message.created_at.timestamp()
+    human_readable_stop_time = ctx.message.created_at.strftime("%H:%M:%S")
+    await ctx.send(f'Sessions stopped at {human_readable_stop_time}')
+
+
+@tasks.loop(seconds=MAX_SESSION_TIME, count=17)
+async def break_reminder():
+    # Ignore the first execution because it happens instantly
+    if break_reminder.current_loop == 0:
+        return
+
+    channel = bot.get_channel(STUDYING_CHANNEL_ID)
+    await channel.send(f"**Take a break!** You have been studying for {MAX_SESSION_TIME*break_reminder.current_loop} minutes")
 
 
 @bot.event
@@ -31,17 +102,10 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    # Bot responses to user inputs
-    if channel == "general":
-        if user_message.lower() == "hello" or user_message.lower() == "hi":
-            await message.channel.send(f'Hello {username}')
-        elif user_message.lower() == "bye":
-            await message.channel.send(f'Bye {username}')
-        elif user_message.lower() == "tell me a joke":
-            await message.channel.send(random.choice(jokes))
-
     # Deletes messages that contain curse words
-    if any(word in user_message for word in curseWords):
+    if any(word in user_message.lower() for word in curseWords):
         await message.delete()
+
+    await bot.process_commands(message)
 
 bot.run(token)
